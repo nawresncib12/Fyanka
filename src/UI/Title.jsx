af386f82c3ef91bd68c4463ed28b0bbbbcb524a8
nawresncib12 <nawresncib@insat.u-carthage.tn>
2022-04-18 16:30:16 +0100

    Updates

--- src/UI/Title.jsx
+++
@@ -1,7 +0,0 @@
-import classes from "./Title.module.css";
-const Title = (props) => {
-  const width = props.width||"";
-  return <div className={classes.title} style={{ width : width}}>{props.children}</div>;
-};
-
-export default Title;
--- src/components/PageContainer.jsx
+++
@@ -1,6 +0,0 @@
-import classes from "./PageContainer.module.css";
-const PageContainer = (props) => {
-  return <>{props.children}</>;
-};
-
-export default PageContainer;
